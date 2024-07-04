36356b5c03cde5c7dafd533c7cc1ddf8e934bed5
criztiandev <criztiandev@gmail.com>
2024-07-04 16:27:21 +0800

    Fixed

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import rantRoutes from "./routes/rant.routes";
 import dotenv from "dotenv";
 import cors from "cors";
-import { notFound, errorHandler } from "./middleware/errorhandler.middleware";
 
 dotenv.config();
 
@@ -15,9 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/rant", rantRoutes);
-
-// app.use(notFound);
-// app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server is running on: ${PORT}`));
 
 export default app;
