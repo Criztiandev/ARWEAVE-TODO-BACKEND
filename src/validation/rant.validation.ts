80a8c4d87a1c61e7c5f1a09ec7af71ceb04030ab
criztiandev <criztiandev@gmail.com>
2024-07-04 15:59:18 +0800

    Added something

--- src/validation/rant.validation.ts
+++ src/validation/rant.validation.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const rantValidationSchema = z.object({
   rant: z
     .string()
-    .min(25, "Rant is too short")
+    .min(25, "Rant is too shorty")
     .max(500, "Rant cannot exceed 500 characters"),
   toc: z.boolean().refine((value) => value === true, {
     message: "You must agree to the terms and conditions",
